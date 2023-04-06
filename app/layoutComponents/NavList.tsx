5bdaf9ad2f6887d307ac3f60ac3a276b2fe67396
misternooblet <dragonroadsupport@gmail.com>
2023-04-07 01:24:15 +0300

    navlist

---
+++ app/layoutComponents/NavList.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import styles from './NavList.module.css';
+import React, { Dispatch, SetStateAction, useRef, useEffect } from 'react';
+
+interface Paths {
+  name: string;
+  path: string;
+}
+
+const NavList = ({
+  paths,
+  setListOpen,
+}: {
+  paths: Paths[];
+  setListOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const wrapperRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        wrapperRef.current &&
+        !wrapperRef.current.contains(event.target as Node)
+      ) {
+        setListOpen(false);
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [wrapperRef]);
+
+  return (
+    <ul ref={wrapperRef} className={styles.navList}>
+      {paths.map((item) => (
+        <li key={item.name}>
+          <Link href={item.path}>{item.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavList;
--- app/layoutComponents/ProfileBox.tsx
+++ app/layoutComponents/ProfileBox.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import styles from './ProfileBox.module.css';
 import { BsFillPersonFill } from 'react-icons/Bs';
+import NavList from './NavList';
+const settings = [
+  { name: 'Profile', path: '/profile' },
+  { name: 'Logout', path: '' },
+];
+
+const loginSettings = [
+  { name: 'Login', path: '/login' },
+  { name: 'Signup', path: '/register' },
+];
+
 const ProfileBox = () => {
+  const [listOpen, setListOpen] = useState(false);
+
+  const handleOpenList = () => {
+    setListOpen(true);
+  };
+
   return (
     <div>
-      <div title="Open Settings" className={styles.pfpBox}>
+      <div
+        title="Open Settings"
+        className={styles.pfpBox}
+        onClick={handleOpenList}
+      >
         <BsFillPersonFill />
+        {listOpen && <NavList paths={settings} setListOpen={setListOpen} />}
       </div>
     </div>
   );
