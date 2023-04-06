b150cb549df7b70d848afad51eed0af1379a10a4
misternooblet <dragonroadsupport@gmail.com>
2023-04-06 20:15:56 +0300

    resetcss

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import React from 'react';
+
+async function testData() {
+  const response = await axios.get(
+    'https://real-cyan-foal-wear.cyclic.app/api/v1/subcategories/642de65687254225aacf39a9',
+    {
+      // query URL without using browser cache
+      headers: {
+        'Cache-Control': 'no-cache',
+        Pragma: 'no-cache',
+        Expires: '0',
+      },
+    }
+  );
+  return response.data.data;
+}
+
+const page = async () => {
+  const data = await testData();
+  console.log(data);
+  return (
+    <div>
+      {data.children.map((child: string) => (
+        <div>{child}</div>
+      ))}
+    </div>
+  );
+};
+
+export default page;
