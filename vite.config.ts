106e8ef40604c538a341e8333480eecfd463218a
misternooblet <dragonroadsupport@gmail.com>
2023-04-19 02:59:46 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,38 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Handy Dandy',
+        short_name: 'Handy Dandy',
+        description: 'Handy Dandy is a wiki for the modern age.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,svg,woff,woff2,ttf,otf}'],
+      },
+    }),
+  ],
 });
